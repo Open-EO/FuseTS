640f70a717196dc2280b211ffea238ac0db2f004
Stefaan Lippens <stefaan.lippens@vito.be>
2022-04-13 09:23:15 +0200

    Issue #44 move package version from setup.cfg to setup.py for jenkinslib
    jenkinslib does not support version management from setup.cfg currently
    implement package version also "single-source" style based on
    __init__.py

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
+import re
+from pathlib import Path
+
 from setuptools import setup
+
+
+def get_version(path: str = "src/fusets/__init__.py"):
+    # Single-sourcing package version (https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#single-sourcing-the-package-version)
+    try:
+        regex = re.compile(r"^__version__\s*=\s*(?P<q>['\"])(?P<v>.+?)(?P=q)\s$", flags=re.MULTILINE)
+        with (Path(__file__).absolute().parent / path).open("r") as f:
+            return regex.search(f.read()).group("v")
+    except Exception:
+        raise RuntimeError("Failed to find version string.")
+
 
 if __name__ == "__main__":
     # See setup.cfg
-    setup()
+    setup(
+        version=get_version(),
+    )

6f133684dd70cf3ee834a2bf8ef367a94585ae77
janssenb <bram.janssen@vito.be>
2023-05-31 13:22:02 +0200

    fix: updated mogpr implementation

--- src/fusets/openeo/mogpr_udf.py
+++ src/fusets/openeo/mogpr_udf.py
@@ -1,4 +1,6 @@
+import os
 import sys
+from configparser import ConfigParser
 from pathlib import Path
 from typing import Dict
 
@@ -15,6 +17,30 @@ def load_venv():
             sys.path.insert(0, venv_path)
 
 
+def set_home(home):
+    os.environ['HOME'] = home
+
+def create_gpy_cfg():
+    home = os.getenv('HOME')
+    set_home('/tmp')
+    user_file = Path.home() / '.config' / 'GPy' / 'user.cfg'
+    if not user_file.exists():
+        user_file.parent.mkdir(parents=True, exist_ok=True)
+    return user_file, home
+
+
+def write_gpy_cfg():
+    user_file, home = create_gpy_cfg()
+    config = ConfigParser()
+    config['plotting'] = {
+        'library': 'none'
+    }
+    with open(user_file, 'w') as cfg:
+        config.write(cfg)
+        cfg.close()
+    return home
+
+
 def apply_datacube(cube: XarrayDataCube, context: Dict) -> XarrayDataCube:
     """
     Apply mogpr integration to a datacube.
@@ -24,9 +50,12 @@ def apply_datacube(cube: XarrayDataCube, context: Dict) -> XarrayDataCube:
     @return:
     """
     load_venv()
+    home = write_gpy_cfg()
 
     from fusets.mogpr import MOGPRTransformer
-    return XarrayDataCube(MOGPRTransformer().fit_transform(cube.get_array()))
+    result = XarrayDataCube(MOGPRTransformer().fit_transform(cube.get_array().to_dataset(dim='bands')))
+    set_home(home)
+    return result
 
 
 def load_mogpr_udf() -> str:

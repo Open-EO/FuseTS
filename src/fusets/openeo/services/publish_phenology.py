2be516c7daebf2b997196c678422a77bd90960f4
janssenb <bram.janssen@vito.be>
2023-06-05 13:53:15 +0200

    chore: start of the phenology notebook

--- src/fusets/openeo/services/publish_phenology.py
+++ src/fusets/openeo/services/publish_phenology.py
@@ -1,6 +1,6 @@
 # Reads contents with UTF-8 encoding and returns str.
 from openeo.api.process import Parameter
-from openeo.processes import apply_dimension, run_udf
+from openeo.processes import apply_dimension, run_udf, apply_neighborhood, add_dimension, reduce_dimension
 
 from fusets.openeo.phenology_udf import load_phenology_udf
 from fusets.openeo.services.helpers import publish_service, read_description
@@ -9,8 +9,44 @@ from fusets.openeo.services.helpers import publish_service, read_description
 def generate_phenology_udp():
     description = read_description('phenology')
 
+    phenology_bands = [
+        "pos_values",
+        "pos_times",
+        "mos_values",
+        "vos_values",
+        "vos_times",
+        "bse_values",
+        "aos_values",
+        "sos_values",
+        "sos_times",
+        "eos_values",
+        "eos_times",
+        "los_values",
+        "roi_values",
+        "rod_values",
+        "lios_values",
+        "sios_values",
+        "liot_values",
+        "siot_values"
+    ]
+
     input_cube = Parameter.raster_cube()
-    process = apply_dimension(input_cube, process=lambda x: run_udf(x, udf=load_phenology_udf(), runtime="Python"), dimension='t')
+    # process = reoduce_dimension(dimension='t', data=input_cube,
+    #                            reducer=lambda x: run_udf(x, udf=load_phenology_udf(), runtime="Python"))
+
+    size = 64
+    overlap = 32
+    process = apply_neighborhood(data=input_cube, process=lambda x: run_udf(x, udf=load_phenology_udf(), runtime="Python"),
+    size=[
+        {'dimension': 'x', 'value': size, 'unit': 'px'},
+        {'dimension': 'y', 'value': size, 'unit': 'px'}
+    ],
+    overlap=[
+        {'dimension': 'x', 'value': overlap, 'unit': 'px'},
+        {'dimension': 'y', 'value': overlap, 'unit': 'px'}
+    ])
+
+    process = add_dimension(data=process, name='phenology', label=phenology_bands)
 
     return publish_service(id="phenology", summary='',
                            description=description, parameters=[

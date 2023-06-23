0b3a3b6120a860b31aa39691df551e4e261a04ee
janssenb <bram.janssen@vito.be>
2023-06-23 08:54:57 +0200

    fix: updated mogpr to use apply_neighborhood

--- src/fusets/openeo/services/publish_mogpr.py
+++ src/fusets/openeo/services/publish_mogpr.py
@@ -1,6 +1,6 @@
 # Reads contents with UTF-8 encoding and returns str.
 from openeo.api.process import Parameter
-from openeo.processes import apply_dimension, run_udf
+from openeo.processes import apply_neighborhood
 
 from fusets.openeo import load_mogpr_udf
 from fusets.openeo.services.helpers import read_description, publish_service
@@ -10,7 +10,13 @@ def generate_mogpr_udp():
     description = read_description('mogpr')
 
     input_cube = Parameter.raster_cube()
-    process = apply_dimension(input_cube, process=lambda x: run_udf(x, udf=load_mogpr_udf(), runtime="Python"), dimension="t")
+    size = 32
+    process = apply_neighborhood(input_cube,
+                                 lambda data: data.run_udf(udf=load_mogpr_udf(), runtime='Python', context=dict()),
+                                 size=[
+                                     {'dimension': 'x', 'value': size, 'unit': 'px'},
+                                     {'dimension': 'y', 'value': size, 'unit': 'px'}
+                                 ], overlap=[])
 
     return publish_service(id="mogpr", summary="Integrates timeseries in data cube using multi-output gaussian "
                                                "process regression.", description=description, parameters=[

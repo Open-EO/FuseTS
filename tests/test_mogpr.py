753026c3b21ca7c5e5feca1517419b4730d78985
Jeroen Dries <jeroen.dries@vito.be>
2023-08-10 16:24:27 +0200

    #79 fix variable naming to be openEO compliant, fix DataSet to DataArray conversions

--- tests/test_mogpr.py
+++ tests/test_mogpr.py
@@ -11,14 +11,28 @@ from fusets.whittaker import  whittaker
 
 
 
+
+ds = xarray.load_dataset(io.BytesIO(requests.get("https://artifactory.vgt.vito.be/testdata-public/fusets/b4_b8_vv_vh/rape.nc",stream=True).content))
+
+ds['RVI'] = (ds.VH + ds.VH) / (ds.VV + ds.VH)
+ds['NDVI'] = (ds.B08 - ds.B04) / (ds.B04 + ds.B08)
+vars = ds[['NDVI', 'RVI']]
+
+def test_mogpr_udf():
+    """
+    Simple test to help debug udf
+    Returns:
+
+    """
+
+    from fusets.openeo.mogpr_udf import apply_datacube
+    from openeo.udf import XarrayDataCube
+    result = apply_datacube(XarrayDataCube(vars.to_array(dim="bands")),context={})
+    print(result)
+    assert result.array.dims == ("bands","t","y","x")
+    assert result.array.shape == (2, 374, 19, 21)
+
 def test_mogpr_train_model():
-    ds = xarray.load_dataset(io.BytesIO(requests.get("https://artifactory.vgt.vito.be/testdata-public/fusets/b4_b8_vv_vh/rye.nc",stream=True).content))
-
-    ds['RVI'] = (ds.VH + ds.VH) / (ds.VV + ds.VH)
-    ds['NDVI'] = (ds.B08 - ds.B04) / (ds.B04 + ds.B08)
-    vars = ds[['NDVI', 'RVI']]
-
-
     t = MOGPRTransformer()
     t.fit(vars)
     out = t.transform(vars)

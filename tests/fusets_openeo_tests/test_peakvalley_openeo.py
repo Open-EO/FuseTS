6e3765f23ce6e504f48ee9fda42e84b26fd0647b
Jeroen Dries <jeroen.dries@vito.be>
2023-11-03 16:06:28 +0100

    add dependencies for peakvalley

--- tests/fusets_openeo_tests/test_peakvalley_openeo.py
+++ tests/fusets_openeo_tests/test_peakvalley_openeo.py
@@ -6,4 +6,9 @@ def test_peakvalley_openeo_udf(wetland_sentinel2_ndvi):
 
     result = peakvalley(wetland_sentinel2_ndvi)
 
-    result.execute_batch("peakvalley.nc",out_format="netCDF",title="Peak-Valley test",description="Testing fusets peak-valley")
+    result.execute_batch("peakvalley.nc",out_format="netCDF",title="Peak-Valley test",description="Testing fusets peak-valley",job_options={
+        'udf-dependency-archives': [
+            'https://artifactory.vgt.vito.be:443/auxdata-public/ai4food/fusets_venv.zip#tmp/venv',
+            'https://artifactory.vgt.vito.be:443/auxdata-public/ai4food/fusets.zip#tmp/venv_static'
+        ]
+})

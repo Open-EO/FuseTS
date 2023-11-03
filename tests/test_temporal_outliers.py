4127f3c8af0227e6d3aa0d48bf2824456f9a2e98
Matic Lubej <matic.lubej@sinergise.com>
2023-11-03 11:46:06 +0100

    add test for temporal outliers

---
+++ tests/test_temporal_outliers.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from fusets._xarray_utils import _extract_dates
+from fusets.temporal_outliers import temporal_outliers_f
+from numpy.testing import assert_almost_equal
+
+
+def test_temporal_outlier_filtering(outlier_timeseries):
+    dates = np.array(_extract_dates(outlier_timeseries))
+    vals = outlier_timeseries.values
+
+    filtered_vals = temporal_outliers_f(dates, vals, window='20D', threshold=3)
+
+    assert_almost_equal(filtered_vals.mean(), 0.09904716, decimal=6)
+    assert_almost_equal(filtered_vals.std(), 0.71552783, decimal=6)
+

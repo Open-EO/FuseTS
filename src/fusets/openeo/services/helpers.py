8d35e07fb9b98adab6680f425a8009a291241ebf
JANSSENB <bram.janssen@vito.be>
2023-12-19 15:44:44 +0100

    feat(#119): first prototype of S1 S2 data fusion UDP

--- src/fusets/openeo/services/helpers.py
+++ src/fusets/openeo/services/helpers.py
@@ -45,3 +45,47 @@ def publish_service(id: str, summary: str, description: str, parameters: list, p
         process_graph=process_graph,
         public=True,
     )
+
+
+DATE_SCHEMA = {
+    "type": "array",
+    "subtype": "temporal-interval",
+    "minItems": 2,
+    "maxItems": 2,
+    "items": {
+        "anyOf": [
+            {
+                "type": "string",
+                "format": "date-time",
+                "subtype": "date-time"
+            },
+            {
+                "type": "string",
+                "format": "date",
+                "subtype": "date"
+            },
+            {
+                "type": "string",
+                "subtype": "year",
+                "minLength": 4,
+                "maxLength": 4,
+                "pattern": "^\\d{4}$"
+            },
+            {
+                "type": "null"
+            }
+        ]
+    },
+    "examples": [
+        [
+            "2015-01-01T00:00:00Z",
+            "2016-01-01T00:00:00Z"
+        ],
+        [
+            "2015-01-01",
+            "2016-01-01"
+        ]
+    ]
+}
+
+GEOJSON_SCHEMA = {"type": "object", "subtype": "geojson"}
